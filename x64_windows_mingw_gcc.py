13a21a5a52a298277c5fbfe1c787507778955b5d
Seokgeun Hong <seokgeun.hong@gmail.com>
2020-01-15 20:25:15 +0900

    Release scons-toolchain-0.1.0.dev-1
    Tested on SCons 3.0
    
    Features:
    
      - Define toolchains, which defines tools and arguments to build.
      - Select toolchains at runtime via SCons arguments
      - Combine toolchains at runtime via SCons arguments
    
    Predefined toolchains:
    
      x64_linux_gcc.py:
        - x64-linux-gcc
        - x64-linux-gcc-7
        - x64-linux-gcc-8
    
      x64_windows_mingw_gcc.py:
        - x64-windows-mingw-gcc
    
      x64_windows_msys_gcc.py:
        - x64-windows-msys-gcc
    
    Predefined toolchain options:
    
      gcc_options.py:
        - gcc-debug
        - gcc-warnings

--- x64_windows_mingw_gcc.py
+++ x64_windows_mingw_gcc.py
@@ -1,11 +1,9 @@
 
 from toolchains import Toolchain
 
-# gcc-mingw-w64-x86-64
-# g++-mingw-w64-x86-64
-#
+
 Toolchain(
-    id='x64-windows-mingw-gcc',
+    'x64-windows-mingw-gcc',
     prefix='x86_64-w64-mingw32-',
     env={
         'CC':'gcc',
